923cc91164690cb68beb0b6851eaf2afa6550f3a
demoncowgirl <bethsalvatore2018@gmail.com>
2019-02-05 06:54:29 -0500

    fixes issue with php artisan serve

--- config/app.php
+++ config/app.php
@@ -163,7 +163,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Illuminate\Foundation\Application::class,
          // this is needed for Laravel Collective to work
         Collective\Html\HtmlServiceProvider::class,
         /*
